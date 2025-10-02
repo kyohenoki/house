4e30338657abd6bc4f41423d69581d5014b99c01
kyohei <kyohenoki@gmail.com>
2025-10-02 11:27:33 +0900

    調整

--- README.md
+++ README.md
@@ -9,3 +9,29 @@
 - [ ] Workersで動くようにする
 
 - [ ] 一通り完成したら記事を書いて公開する
+
+### 開発
+
+[LocalStack](https://www.localstack.cloud/) というR2互換S3のサーバーを立ち上げる
+
+```
+docker compose up
+```
+
+今は `ageru` という名前の R2 に記事や JSON や最適化した画像をアップロードして、生成のリクエストや Git コミットも簡単に出来るツールを作ってる
+
+```
+bun ageru [cmd]
+```
+
+Biome と Prettier（ESLint はいつか入れたいかも）
+
+```
+bun fc
+```
+
+`build` と `preview`（まだ動かない）
+
+```
+bun start
+```
--- src/katachi/menu.tsx
+++ src/katachi/menu.tsx
@@ -1,8 +1,5 @@
 import { atom, useAtom, useAtomValue } from 'jotai'
 
-// const menu = atom(true)
-const menu = atom(false)
-
 export function Menu() {
   const [open, setOpen] = useAtom(menu)
   return (
@@ -14,11 +11,14 @@ export function Menu() {
   )
 }
 
+// const menu = atom(true)
+const menu = atom(false)
+
 export function Komoku() {
   const open = useAtomValue(menu)
   if (open) {
     return (
-      <div className="absolute right-0 flex flex-col items-end rounded-bl-2xl border-[2px] border-[var(--be)] bg-[var(--haikei)] pb-1 pl-3 pr-3 pt-3 text-[var(--bemo)]">
+      <div className="absolute right-0 flex flex-col items-end rounded-bl-xl border-[1.5px] border-[var(--be)] bg-[var(--haikei)] pl-3 pr-3 pt-2 text-[var(--bemo)]">
         <Aaa href="/me">私について</Aaa>
         <Aaa href="/kiroku">記録帳</Aaa>
         <Aaa href="/mada">これから</Aaa>
